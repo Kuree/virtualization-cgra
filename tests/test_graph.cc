333571640288c0682f7ef84d4c6552484693470b
Keyi Zhang <keyi@stanford.edu>
2020-09-28 23:58:53 -0700

    clean up test and cmake

--- tests/test_graph.cc
+++ tests/test_graph.cc
@@ -22,21 +22,14 @@ TEST(multi_graph, merge_cascade_edge) {  // NOLINT
     auto sv_m58 = mg.find(m58);
     EXPECT_NE(sv_m58, nullptr);
     auto se = *(sv_m58->edges_from.begin());
-    auto num_edges_m58 = sv_m58->edges_to.size();
 
     EXPECT_EQ(se->from->vertices.size(), 1);
     auto io = *(se->from->vertices.begin());
     auto s_io = mg.find(io);
-    auto num_edges_io = s_io->edges_to.size();
 
     EXPECT_NE(s_io, nullptr);
     auto io_name = io->name;
     EXPECT_EQ(io_name, "I20");
-
-    // merge that edge
-    mg.merge(se);
-    EXPECT_EQ(s_io, mg.find(m58));
-    EXPECT_EQ(s_io->edges_to.size(), num_edges_io + num_edges_m58 - 1);
 }
 
 TEST(multi_graph, merge_duplicated_edge) {
